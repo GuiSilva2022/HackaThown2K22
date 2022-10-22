5a4b5f8a46995bf2c5f2330781b660de01b4e002
GuiSilva2022 <gui.carlossrs2018@gmail.com>
2022-10-22 06:05:30 -0300

    Programação codigo morse

---
+++ sketch_oct22a.ino
@@ -0,0 +1,136 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(7, OUTPUT);
+pinMode(6, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+if(digitalRead(6) == HIGH)
+{
+
+   digitalWrite(7,HIGH); //A
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+   digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+   digitalWrite(7,HIGH); //C
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(400);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+  
+   digitalWrite(7,HIGH); //A
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+   digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+   digitalWrite(7,HIGH); //O
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+   digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+  digitalWrite(7,HIGH); //E
+  delay(400);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+  digitalWrite(7,HIGH); //R
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+
+  delay(1200);
+
+  digitalWrite(7,HIGH); //E
+  delay(400);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+  digitalWrite(7,HIGH); //A
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+   digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+   digitalWrite(7,HIGH); //C
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+  digitalWrite(7,HIGH);
+  delay(400);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+  digitalWrite(7,HIGH); //A
+  delay(400);
+  digitalWrite(7,LOW);
+  delay(600);
+   digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+
+  delay(1200);
+
+  digitalWrite(7,HIGH); //O
+  delay(1200);
+  digitalWrite(7,LOW);
+  delay(600);
+   digitalWrite(7,HIGH);
+  delay(1200);
+  digitalWrite(7,LOW);
+  
+}
+}
